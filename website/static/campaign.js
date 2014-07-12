e250660be4648d8e10cc1ae6abb5f73cbbf847f5
Alexis <lxslarry@gmail.com>
2014-07-12 05:05:32 -0700

    This version has the ability to create a campaign, input it in Parse, and then display the campaign.

--- website/static/campaign.js
+++ website/static/campaign.js
@@ -27,10 +27,46 @@ $(function () {
                         console.log("not signed up, some error " + error.code + " " + error.message);
                     }
                 });
-            }});
+            }
+        });                
+    });
 
- 
-                
+    var Campaign = Parse.Object.extend("Campaign");
+
+    $('#create_campaign_form').on('submit', function(e) {
+        e.preventDefault();
+        var title = $('#title').val();
+        var description = $('#description').val();
+        //var image = $('#image').val();
+        var start_date = $('#start_date').val();
+        var end_date = $('#end_date').val();
+        var expiration = $('#expiration').val()
+        var youngestAge = $('#youngestAge').val()
+        var oldestAge = $('#oldestAge').val()
+        var state = $('#state').val()
+        var i1 = $('#i1').val()
+        var campaign = new Campaign();
+        
+        campaign.set("title", title);
+        campaign.set("description", description);
+        //campaign.set("image", image);
+        campaign.set("start_date", start_date);
+        campaign.set("end_date", end_date);
+        campaign.set("expiration", expiration);
+        campaign.set("youngestAge", parseInt(youngestAge));
+        campaign.set("oldestAge", parseInt(oldestAge));
+        campaign.set("state", state);
+        campaign.set("i1", i1);
+
+        campaign.save(null, {
+            success: function(campaign) {
+                console.log('This campaign was saved')
+                var id = campaign.id
+                window.open("/viewCampaign?id=" + id, "_self");
+            }, error: function(campaign, error) {
+                console.log("some error " + error.code + " "  + error.message);
+            }
+        });
     });
 
 });
