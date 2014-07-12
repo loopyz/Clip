26ec63115d16548657641e5902e1fe4d369dfd4c
Niveditha Jayasekar <jnive05@gmail.com>
2014-07-11 19:34:57 -0700

    initial xcodeproj + gitignore

---
+++ clip/clipTests/clipTests.m
@@ -0,0 +1,34 @@
+//
+//  clipTests.m
+//  clipTests
+//
+//  Created by Niveditha Jayasekar on 7/11/14.
+//  Copyright (c) 2014 Niveditha Jayasekar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface clipTests : XCTestCase
+
+@end
+
+@implementation clipTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
