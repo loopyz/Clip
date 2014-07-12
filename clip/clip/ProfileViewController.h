7e74d35c66eadf3381b10501b6236dedb9da6b91
Lucy Guo <lguo@andrew.cmu.edu>
2014-07-12 03:25:29 -0700

    profile view setup

--- clip/clip/ProfileViewController.h
+++ clip/clip/ProfileViewController.h
@@ -6,9 +6,18 @@
 //  Copyright (c) 2014 Niveditha Jayasekar. All rights reserved.
 //
 #import "KSVideoPlayerView.h"
+#import <FacebookSDK/FacebookSDK.h>
 
 #import <UIKit/UIKit.h>
 
 @interface ProfileViewController : UIViewController
+
 @property (nonatomic, strong) KSVideoPlayerView *player;
+@property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UITableView *videosTable;
+@property (nonatomic, strong) UIView *profileSnippetView;
+@property (nonatomic, strong) FBProfilePictureView *fbProfilePic;
+@property (nonatomic, strong) NSString *userid;
+
+
 @end
